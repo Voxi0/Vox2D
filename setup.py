5d93bcdf206aeb78b55257d87909d40a14e738d8
Voxi0 <alif200099@gmail.com>
2022-09-17 10:10:10 +0600

    Updated LICENSE.txt, setup.py and README.txt. Added icon.ico

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ target = Executable(
 )
 
 setup(
-    name="Minecraft 2D",
-    version=1.0,
-    description="2D Minecraft Clone in Ursina.",
-    author = "BitterSweet",
+    name="Vox2D",
+    version=1.1,
+    description="2D-Voxel Game Made With Python and Ursina.",
+    author = "Voxi0",
     options={"build_exe": {"packages": ["ursina"]}},
     executables = [target]
 )
